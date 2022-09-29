00d0cb8113cdc8d1191d5612921efa3dcdedca70
Luis Angel <Luisparr14@gmail.com>
2022-09-29 17:42:07 -0500

    increment timeout

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,4 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL =
   "http://ec2-44-201-108-206.compute-1.amazonaws.com/decameron/api";
 
+axios.defaults.timeout = 120000;
+
 export default axios;
