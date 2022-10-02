a109a8d8170b7161c4742db7f56df5f07fc1b414
Luis Angel <Luisparr14@gmail.com>
2022-10-02 07:43:24 -0500

    Add methods to room service

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -14,3 +14,18 @@ export const getRoomTypes = async () => {
   const { data: roomTypesData } = await axios.get("/room-types");
   return roomTypesData;
 };
+
+export const createRoom = async (room) => {
+  const { data: roomData } = await axios.post("/rooms", room);
+  return roomData;
+};
+
+export const updateRoom = async (idRoom, room) => {
+  const { data: roomData } = await axios.put(`/rooms/${idRoom}`, room);
+  return roomData;
+};
+
+export const deleteRoom = async (idRoom) => {
+  const { data: roomData } = await axios.delete(`/rooms/${idRoom}`);
+  return roomData;
+};
