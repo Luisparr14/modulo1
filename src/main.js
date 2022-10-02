eba99bbc80093059414475be58510d330383742a
Luis Angel <Luisparr14@gmail.com>
2022-10-02 07:42:27 -0500

    Install and configure sweet alert2 vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import VueSweetalert2 from "vue-sweetalert2";
 import App from "./App.vue";
 import router from "./router";
 
@@ -9,9 +9,11 @@ import router from "./router";
 import "normalize.css";
 import "admin-lte/dist/js/adminlte.min.js";
 import "admin-lte/dist/css/adminlte.min.css";
+import "sweetalert2/dist/sweetalert2.min.css";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueSweetalert2);
 
 app.mount("#app");
