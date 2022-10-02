c0a2f6b51fd8f8323b982bf9a2f000d15edbf8f6
Luis Angel <Luisparr14@gmail.com>
2022-10-02 07:43:12 -0500

    Add methods to hotel service

--- src/services/hotels.js
+++ src/services/hotels.js
@@ -9,3 +9,18 @@ export const getHotel = async (id) => {
   const { data: hotelData } = await axios.get(`/hotels/${id}`);
   return hotelData;
 };
+
+export const createHotel = async (hotel) => {
+  const { data: hotelData } = await axios.post("/hotels", hotel);
+  return hotelData;
+};
+
+export const updateHotel = async (id, hotel) => {
+  const { data: hotelData } = await axios.put(`/hotels/${id}`, hotel);
+  return hotelData;
+};
+
+export const deleteHotel = async (id) => {
+  const { data: hotelData } = await axios.delete(`/hotels/${id}`);
+  return hotelData;
+};
