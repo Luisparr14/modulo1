2423eada87daa247bb384e9da156a30b145a1a71
Luis Angel <Luisparr14@gmail.com>
2022-10-02 08:58:27 -0500

    Creation of header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-white shadow">
+    <div class="py-4 px-2 fw-bold">Panel de Administración</div>
+  </header>
+</template>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,5 +1,9 @@
+<script setup>
+import HeaderComponent from "../components/HeaderComponent.vue";
+</script>
 <template>
   <section class="px-2">
+    <HeaderComponent />
     <slot></slot>
   </section>
 </template>
